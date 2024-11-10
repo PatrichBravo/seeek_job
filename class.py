deae6175d58dde72485b0889765cacdbd9cd62fc
PatrichBravo <patrich.bravo@gmail.com>
2024-11-09 17:18:10 -0800

    Configuration done

---
+++ class.py
@@ -0,0 +1,70 @@
+import pyautogui
+from time import sleep
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait as W
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as E
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.alert import Alert
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.chrome.service import Service as ChromeService
+from src.utils import chrome_browser_options
+import csv 
+from src.user_config import work
+
+class BrowserAutomation:
+    def __init__(self):
+        try:
+            self.options = chrome_browser_options()  # Configuración personalizada
+            self.service = ChromeService(ChromeDriverManager().install())
+            self.driver = None
+        except Exception as e:
+            raise RuntimeError(f"Failed to initialize browser setup: {str(e)}")
+
+    def init_browser(self):
+        try:
+            self.driver = webdriver.Chrome(service=self.service, options=self.options)
+        except Exception as e:
+            raise RuntimeError(f"Failed to initialize browser: {str(e)}")
+
+    def loginn(self):
+        if not self.driver:
+            raise RuntimeError("Browser not initialized. Call init_browser() first.")
+        
+        self.driver.get('https://login.seek.com/login?state=hKFo2SAzLWdoSVBpNnJleWp6cEZ0cW1IeFZJbU9ZTTFJS2FaRKFupWxvZ2luo3RpZNkgR3hJZ3loYmdiazN3a2hHeWNqU0RnR2p6dU5QWjk0RGOjY2lk2SB5R0JWZ2U2Nks1TkpwU041dTcxZlU5MFZjVGxFQVNOdQ&client=yGBVge66K5NJpSN5u71fU90VcTlEASNu&protocol=oauth2&redirect_uri=https%3A%2F%2Fwww.seek.com.au%2Foauth%2Fcallback%2F&scope=openid%20profile%20email%20offline_access&audience=https%3A%2F%2Fseek%2Fapi%2Fcandidate&fragment=%2Foauth%2Flogin%3Flocale%3Dau%26language%3Den%26realm%3DUsername-Password-Authentication%26da_cdt%3Dvisid_01931366de6c000523dae529aa3305075003906d00942-sesid_1731197329007-hbvid_81b41409_b72c_4aed_a211_9c0183ec5bf1-tempAcqSessionId_1731197329009-tempAcqVisitorId_81b41409b72c4aeda2119c0183ec5bf1&ui_locales=en&JobseekerSessionId=cf618dba-afe0-4ff5-8d44-7a8c9be7a72d&language=en-AU&response_type=code&response_mode=query&nonce=c2NVQkxQbjl6MzA1VC5TUzZhUFYwdnNFN1pjU1JEZkhVZGNrOXVTcS5xRw%3D%3D&code_challenge=izRMkFftM_hVrAUkIceIiJCaIVHj6P-eFBUXstHI_d4&code_challenge_method=S256&auth0Client=eyJuYW1lIjoiYXV0aDAtc3BhLWpzIiwidmVyc2lvbiI6IjEuMjIuMyJ9#/login?locale=au&language=en&realm=Username-Password-Authentication&da_cdt=visid_01931366de6c000523dae529aa3305075003906d00942-sesid_1731197329007-hbvid_81b41409_b72c_4aed_a211_9c0183ec5bf1-tempAcqSessionId_1731197329009-tempAcqVisitorId_81b41409b72c4aeda2119c0183ec5bf1')  # URL completo
+        self.driver.maximize_window()
+        
+        input('Press enter when you log in..!!')
+
+        sleep(5)
+        
+        wait_time_out = 15
+        wait_variable = W(self.driver, wait_time_out)
+        print("Login completed and browser maximized!")
+        
+    def search_jobs(self):
+        self.wait_time_out = 15
+        self.wait_variable = W(self.driver, self.wait_time_out)
+        keyword = self.driver.find_element("xpath","//input[@id='keywords-input']" )
+        keyword.send_keys(work)
+        sleep(2)
+        
+        location = self.driver.find_element("xpath","//input[@id='keywords-input']" )
+        location.send_keys(location)
+        sleep(2)
+        
+    def scraper_job(self):
+        
+    def apply_jobs(self):
+        
+
+
+# Uso de la clase
+if __name__ == "__main__":
+    automation = BrowserAutomation()
+    automation.init_browser()
+    automation.loginn()
---
+++ src/user_config.py
@@ -0,0 +1,2 @@
+work = "Data analyst"
+location = "Australia"
