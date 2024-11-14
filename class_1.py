774f5296731f882fa72bd928098a739858506e26
PatrichBravo <patrich.bravo@gmail.com>
2024-11-14 01:06:11 -0800

    Fixing questions bugs

--- class_1.py
+++ class_1.py
@@ -82,7 +82,7 @@ class BrowserAutomation:
         
     def scraper_job(self):
         self.rows = []
-        for i in range (1,3):#23
+        for i in range (1,10):#23
             
             try:
                 jobs = self.driver.find_element("xpath", "/html/body/div[1]/div/div[3]/div/section/div[2]/div/div/div[1]/div/div/div[1]/div/div[1]/div[3]/div["+str(i)+"]/article/div[2]/a")
@@ -119,14 +119,16 @@ class BrowserAutomation:
                         button.click()
                         print("Quick Apply button clicked successfully!")
                         sleep(5)
-                    return  print("No 'Quick Apply' button found.")# Exit after clicking the correct button
-            #------------------ add function apply_jobs----------------------#   
-            
-            #------------------end of the function---------------------------#         
+                    else:  print("No 'Quick Apply' button found.")
+                        # Exit after clicking the correct button
+                   
             except Exception as e:
                 print(f"Could not find or click the Quick Apply button: {e}")
                 sleep(2)
                 pass
+             #------------------ add function apply_jobs----------------------#   
+            BrowserAutomation.apply_jobs(self)          
+            #------------------end of the function---------------------------# 
 
     def apply_jobs(self):
         
@@ -161,15 +163,154 @@ class BrowserAutomation:
             sleep(2)
             cover_letter_dialog.send_keys(response)
             
-            sleep(15)
+            sleep(5)
+            try:
+                press_continue = self.driver.find_element("xpath","/html/body/div[1]/div/div[1]/div/div/div[3]/div[2]/div[4]/div/button")
+                press_continue.click()
+            except:
+                print("i coulnd't find it")
+                pass
+
+            sleep(3)
+#------------------------------------trying what is goning wrong---------------------------------------------
+            try:
+                #Which of the following statements best describes your right to work in Australia?
+                rights = self.driver.find_element(By.ID, "question-AU_Q_6_V_9")
+                #rights.click()
+                print() 
+                rights_to_work = Select(rights)
+                rights_to_work.select_by_value("AU_Q_6_V_9_A_14978")#Require sponsorship or by visible text
+                sleep(1)
+                #---------------Codes---------------------------------        
+                #AU_Q_6_V_9_A_14970	I'm an Australian citizen
+                #AU_Q_6_V_9_A_14971	I'm a permanent resident and/or NZ citizen
+                #AU_Q_6_V_9_A_14972	I have a family/partner visa with no restrictions
+                #AU_Q_6_V_9_A_14973	I have a graduate temporary work visa
+                #AU_Q_6_V_9_A_14974	I have a holiday temporary work visa
+                #AU_Q_6_V_9_A_14975	I have a temporary visa with restrictions on work location (e.g. skilled regional visa 491)
+                #AU_Q_6_V_9_A_14976	I have a temporary protection or safe haven enterprise work visa
+                #AU_Q_6_V_9_A_28629	I have a temporary visa with no restrictions (e.g. doctoral student)
+                #AU_Q_6_V_9_A_14977	I have a temporary visa with restrictions on work hours (e.g. student visa, retirement visa)
+                #AU_Q_6_V_9_A_29143	I have a temporary visa with restrictions on industry (e.g. temporary activity visa 408)
+                #AU_Q_6_V_9_A_14978	I require sponsorship to work for a new employer (e.g. 482, 457)
+            except:
+                pass
             
-            press_continue = self.driver.find_element("xpath","/html/body/div[1]/div/div[1]/div/div/div[3]/div[2]/div[4]/div/button")
-            press_continue.click()
+            try:
+                #How many years' experience do you have as a data analyst?
+                
+                data_analyst_exp = self.driver.find_element(By.ID, "question-AU_Q_6048_V_4") 
+                data_analyst_exp = Select(rights)
+                data_analyst_exp.select_by_value("AU_Q_6048_V_4_A_6054")
+                sleep(1)
+                
+                #AU_Q_6048_V_4_A_6049	No experience
+                #AU_Q_6048_V_4_A_6050	Less than 1 year
+                #AU_Q_6048_V_4_A_6051	1 year
+                #AU_Q_6048_V_4_A_6052	2 years
+                #AU_Q_6048_V_4_A_6053	3 years
+                #AU_Q_6048_V_4_A_6054	4 years
+                #AU_Q_6048_V_4_A_6055	5 years
+                #AU_Q_6048_V_4_A_6056	More than 5 years
+            except:
+                pass
+            try:
+                #Do you have experience working on data migration projects?
+                #yes
+                data_mig_exp = self.driver.find_element(By.ID, "question-AU_Q_970_V_2_0") 
+                #question-AU_Q_970_V_2_0	Yes
+                #question-AU_Q_970_V_2_1	No
+                data_mig_exp.click()
+                sleep(1)
+            except:
+                pass
+            try:
+                #Do you hold Australian Security Clearance?
+                #no
+                aus_seg_clearance = self.driver.find_element(By.ID, "question-AU_Q_176_V_4_4")
+                aus_seg_clearance.click()
+                #question-AU_Q_176_V_4_0	Yes, Baseline
+                #question-AU_Q_176_V_4_1	Yes, NVL1
+                #question-AU_Q_176_V_4_2	Yes, NVL2
+                #question-AU_Q_176_V_4_3	Yes, TSPV
+                #question-AU_Q_176_V_4_4	No
+                sleep(1)
+            except:
+                pass
+            try:
+                #How many years' experience do you have using SQL queries?
+                sql_exp = self.driver.find_element(By.ID, "question-AU_Q_221_V_2") 
+                sql_exp = Select(rights)
+                sql_exp.select_by_value("AU_Q_221_V_2_A_23228")
+                
+                #AU_Q_221_V_2_A_23224	No experience
+                #AU_Q_221_V_2_A_23225	Less than 1 year
+                #AU_Q_221_V_2_A_23226	1 year
+                #AU_Q_221_V_2_A_23227	2 years
+                #AU_Q_221_V_2_A_23228	3 years
+                #AU_Q_221_V_2_A_23229	4 years
+                #AU_Q_221_V_2_A_23230	5 years
+                #AU_Q_221_V_2_A_23231	More than 5 years
+                sleep(1)
+            except:
+                pass
+            try:
+                #Do you have strong Tableau experience?
+
+                #yes
+                tableau_exp = self.driver.find_element(By.ID, "question-indirect_4ad8449d-0db6-432c-a34e-d0695bd98627_4089e634-86be-42be-b847-3bcbf7b500a6_0") 
+                #no
+                #data_mig_exp = self.driver.find_element(By.ID, "question-indirect_4ad8449d-0db6-432c-a34e-d0695bd98627_4089e634-86be-42be-b847-3bcbf7b500a6_1") 
+                tableau_exp.click()
+                sleep(1)
+            except:
+                pass
+            try:
+                
+                #Can you start a new role within 2 weeks?
+                start_2_weeks = self.driver.find_element(By.ID, "question-indirect_4ad8449d-0db6-432c-a34e-d0695bd98627_bb22e15e-af85-486a-a898-e76706528c3e_0") 
+                start_2_weeks.click()
+                sleep(1)
+
+                #question-indirect_4ad8449d-0db6-432c-a34e-d0695bd98627_bb22e15e-af85-486a-a898-e76706528c3e_0	Yes
+                #question-indirect_4ad8449d-0db6-432c-a34e-d0695bd98627_bb22e15e-af85-486a-a898-e76706528c3e_1	No
+                
+            except:
+                pass
+
+
+#------------------------------------end of the code----------------------------------------------------------            
+            
+            
+            
+            #questions_and_answer(self)
+            sleep(5)
+            try:
+                continue_apply = self.driver.find_element("xpath", "/html/body/div[1]/div/div[1]/div/div/div[3]/div/form/div/div[4]/div[1]/button")
+                continue_apply.click()
+                sleep(4)
+            except:
+                print(f"i couldnt find {continue_apply}")
+                pass
+            try:
+                continue_update = self.driver.find_element("xpath", "/html/body/div[1]/div/div[1]/div/div/div[3]/div/div[6]/div[1]/button")
+                continue_update.click()
+                sleep(4)
+            except:
+                pass
+            try:
+                submit_apply = self.driver.find_element("xpath", '//*[@id="app"]/div/div[1]/div/div/div[3]/div/div[5]/div/button')
+                submit_apply.click()
+                sleep(4)
+            except:
+                pass
+            
             
 
             
         except Exception as e:
             print(f"Error while processing job: {e}")
+            pass
     
     def next_page(self):
         return
@@ -239,7 +380,7 @@ if __name__ == "__main__":
         automation.search_jobs()
         automation.scraper_job()
         automation.enter_application_jobs()
-        automation.apply_jobs()
+
         sleep(5)
 
         # Asking ChatGPT
--- src/question_and_answer.py
+++ src/question_and_answer.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as E
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.alert import Alert
 from selenium import webdriver
-from class_1 import BrowserAutomation
+
 
 def questions_and_answer(self):
     try:
@@ -29,25 +29,79 @@ def questions_and_answer(self):
     except:
         pass
     
+    try:
+        #How many years' experience do you have as a data analyst?
+        
+        data_analyst_exp = self.driver.find_element(By.ID, "question-AU_Q_6048_V_4") 
+        data_analyst_exp = Select(rights)
+        data_analyst_exp.select_by_value("AU_Q_6048_V_4_A_6054")
+        
+        #AU_Q_6048_V_4_A_6049	No experience
+        #AU_Q_6048_V_4_A_6050	Less than 1 year
+        #AU_Q_6048_V_4_A_6051	1 year
+        #AU_Q_6048_V_4_A_6052	2 years
+        #AU_Q_6048_V_4_A_6053	3 years
+        #AU_Q_6048_V_4_A_6054	4 years
+        #AU_Q_6048_V_4_A_6055	5 years
+        #AU_Q_6048_V_4_A_6056	More than 5 years
+    except:
+        pass
+    try:
+        #Do you have experience working on data migration projects?
+        #yes
+        data_mig_exp = self.driver.find_element(By.ID, "question-AU_Q_970_V_2_0") 
+        #question-AU_Q_970_V_2_0	Yes
+        #question-AU_Q_970_V_2_1	No
+        data_mig_exp.click()
+    except:
+        pass
+    try:
+        #Do you hold Australian Security Clearance?
+        #no
+        aus_seg_clearance = self.driver.find_element(By.ID, "question-AU_Q_176_V_4_4")
+        aus_seg_clearance.click()
+        #question-AU_Q_176_V_4_0	Yes, Baseline
+        #question-AU_Q_176_V_4_1	Yes, NVL1
+        #question-AU_Q_176_V_4_2	Yes, NVL2
+        #question-AU_Q_176_V_4_3	Yes, TSPV
+        #question-AU_Q_176_V_4_4	No
+    except:
+        pass
+    try:
+        #How many years' experience do you have using SQL queries?
+        sql_exp = self.driver.find_element(By.ID, "question-AU_Q_221_V_2") 
+        sql_exp = Select(rights)
+        sql_exp.select_by_value("AU_Q_221_V_2_A_23228")
+           
+        #AU_Q_221_V_2_A_23224	No experience
+        #AU_Q_221_V_2_A_23225	Less than 1 year
+        #AU_Q_221_V_2_A_23226	1 year
+        #AU_Q_221_V_2_A_23227	2 years
+        #AU_Q_221_V_2_A_23228	3 years
+        #AU_Q_221_V_2_A_23229	4 years
+        #AU_Q_221_V_2_A_23230	5 years
+        #AU_Q_221_V_2_A_23231	More than 5 years
+    except:
+        pass
+    try:
+        #Do you have strong Tableau experience?
+
+        #yes
+        tableau_exp = self.driver.find_element(By.ID, "question-indirect_4ad8449d-0db6-432c-a34e-d0695bd98627_4089e634-86be-42be-b847-3bcbf7b500a6_0") 
+        #no
+        #data_mig_exp = self.driver.find_element(By.ID, "question-indirect_4ad8449d-0db6-432c-a34e-d0695bd98627_4089e634-86be-42be-b847-3bcbf7b500a6_1") 
+        tableau_exp.click()
+    except:
+        pass
+    try:
+        
+        #Can you start a new role within 2 weeks?
+        start_2_weeks = self.driver.find_element(By.ID, "question-indirect_4ad8449d-0db6-432c-a34e-d0695bd98627_bb22e15e-af85-486a-a898-e76706528c3e_0") 
+        start_2_weeks.click()
+
+        #question-indirect_4ad8449d-0db6-432c-a34e-d0695bd98627_bb22e15e-af85-486a-a898-e76706528c3e_0	Yes
+        #question-indirect_4ad8449d-0db6-432c-a34e-d0695bd98627_bb22e15e-af85-486a-a898-e76706528c3e_1	No
+        
+    except:
+        pass
     
-    #How many years' experience do you have as a data analyst?
-    
-    data_analyst_exp = self.driver.find_element(By.ID, "question-AU_Q_6048_V_4") 
-    data_analyst_exp = Select(rights)
-    data_analyst_exp.select_by_value("AU_Q_6048_V_4_A_6054")
-    
-    #AU_Q_6048_V_4_A_6049	No experience
-    #AU_Q_6048_V_4_A_6050	Less than 1 year
-    #AU_Q_6048_V_4_A_6051	1 year
-    #AU_Q_6048_V_4_A_6052	2 years
-    #AU_Q_6048_V_4_A_6053	3 years
-    #AU_Q_6048_V_4_A_6054	4 years
-    #AU_Q_6048_V_4_A_6055	5 years
-    #AU_Q_6048_V_4_A_6056	More than 5 years
-    
-    #Do you have experience working on data migration projects?
-    #yes
-    data_mig_exp = self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div/div[3]/div/form/div/fieldset[1]/div/div/div/div[1]/div/input") 
-    #no
-    #data_mig_exp = self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div/div[3]/div/form/div/fieldset[1]/div/div/div/div[2]/div/input") 
-    data_mig_exp.click()
